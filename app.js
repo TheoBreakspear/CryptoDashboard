845d274ae2cb52b315476c18a5b869311ee6f473
Theo Breakspear <theo.breakspear@gmail.com>
2021-10-15 20:11:38 +0100

    refactored updatePrice() to async/await

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const priceDisplay = document.getElementById("price");
 
-fetch('https://api.coinbase.com/v2/prices/spot?currency=USD')
-.then(
-    response => {
-        if (response.ok){
-            return response.json()
-        }
-        throw new Error(`${response.status} ${response.statusText}`);
-    },
-    networkError => {
-        console.error(networkError.message);
-    }
-)
-.then(
-    jsonResponse => {
-        priceDisplay.innerText = jsonResponse.data.amount;
-    }
-)
-.catch(err => console.error(err))
+const url = 'https://api.coinbase.com/v2/prices/spot?currency=USD'
 
+async function updatePrice() {
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status} ${response.statusText}`);
+        }
+        let jsonResponse = await response.json();
+
+        let price = jsonResponse.data.amount;
+        priceDisplay.innerText = price;
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+let updateInterval = setInterval(updatePrice, 1000);
